ebe8883a9b56d92ca3e1c44be8c79dc9ec487c98
Mlodovico <murilo.lodovico@outlook.com>
2020-04-20 17:10:37 -0300

    1.00.00 finalizado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,69 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  var Likes = 0;
+
+  const repositorie = { id: uuid(), title, url, techs, Likes }
+
+  try {
+    repositories.push(repositorie);
+    console.log('Success');
+  }catch(err) {
+    return response.json(err);
+  }
+
+  return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoditorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+  if(repoditorieIndex < 0) {
+    return response.status(400).json({error: 'This id does not match anyone'});
+  } 
+
+  const repositorie = {
+    title, 
+    url, 
+    techs
+  } = request.body;
+
+  repositories[repoditorieIndex] = repositorie;
+  return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+  if(repositorieIndex < 0) {
+    return response.status(400).json({error: 'This id does not match with anyone'});
+  } 
+
+  repositories.splice(repositorieIndex, 1);
+  return response.status(200).json('Success delete');
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const idIndexRepository = repositories.findIndex(repositorie => repositorie.id === id);
+
+  if(idIndexRepository < 0) {
+    return response.status(400).json({error: 'This id does not match with anyone'});
+  }
+
+  const repositorieLike = repositories.forEach(repositore => repositore.Likes ++);
+
+  return response.json(repositorieLike);
+
 });
 
 module.exports = app;
